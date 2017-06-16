8d789d1e700e2a50966717621172a7fa8e6cdaa1
McKennon McMillian <mbmcmill@go.olemiss.edu>
2017-06-15 23:55:05 -0500

    Update pics.sh

--- pics.sh
+++ pics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# This is used to collect images from a live web cam
+# Any live webcam can be used ass long it is public and you know the url
 for i in `seq 1 300`; do
 wget http://livecam-static.olemiss.edu/union.jpg
 sleep 10
