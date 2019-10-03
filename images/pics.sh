80263d23580146520b4ac6e2665bf5c552af9724
McKennon McMillian <mbmcmill@go.olemiss.edu>
2019-10-03 15:04:17 -0500

    Update pics.sh to take the url for the webcam images as a cammand line argument

--- images/pics.sh
+++ images/pics.sh
@@ -2,6 +2,6 @@
 # This is used to collect images from a live web cam
 # Any live webcam can be used ass long it is public and you know the url
 for i in `seq 1 300`; do
-wget https://livecam-static.olemiss.edu/starbucks.jpg
+wget $1
 sleep 10
 done
